1148ea637573aec5054e2a3b1bc37acde566ed90
Thomas Sankara <thomassankara74@gmail.com>
2022-03-24 17:49:38 +0300

    add context

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "./index.css"
 import App from "./App"
+import { EntriesContextProvider } from "./context/context"
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <EntriesContextProvider>
+      <App />
+    </EntriesContextProvider>
   </React.StrictMode>,
   document.getElementById("root")
 )
