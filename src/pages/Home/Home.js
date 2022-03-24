290d50960f0431126cfe6c316a4c8b8cf5841cc1
Thomas Sankara <thomassankara74@gmail.com>
2022-03-24 14:50:08 +0300

    add absolute background

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,20 +5,19 @@ import logo from "../../images/logo.svg"
 export default function HomeHero() {
   return (
     <>
-      <section className="hero">
-        <div className="overlay flex flex-col items-center justify-center h-screen px-5 2xl:px-0">
-          <img src={logo} alt="Diarry" className="block mx-auto mb-5" />
-          <h1 className="text-white font-bold mb-5 text-4xl md:text-5xl">
-            Diarry
-          </h1>
-          <p className="text-white text-center px-5 mb-10 lg:text-xl">
-            Free, responsive and accessible diary app for you.
-          </p>
-          <Link to="/diarry" className="btn">
-            Test it out
-          </Link>
-        </div>
-      </section>
+      <section className="hero"></section>
+      <div className="relative z-10 flex flex-col items-center justify-center h-screen px-5 2xl:px-0">
+        <img src={logo} alt="Diarry" className="block mx-auto mb-5" />
+        <h1 className="text-white font-bold mb-5 text-4xl md:text-5xl">
+          Diarry
+        </h1>
+        <p className="text-white text-center px-5 mb-10 lg:text-xl">
+          Free, responsive and accessible diary app for you.
+        </p>
+        <Link to="/diarry" className="btn">
+          I want this
+        </Link>
+      </div>
     </>
   )
 }
