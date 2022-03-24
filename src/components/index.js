f7372bfbfeed31fc583431b0a5ba187dbe387eed
Thomas Sankara <thomassankara74@gmail.com>
2022-03-24 17:51:29 +0300

    add NewEntry component

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,6 @@
 import Sidebar from "./Sidebar"
 import Entries from "./Entries"
+import MobileBar from "./MobileBar"
+import NewEntry from "./NewEntry"
 
-export { Sidebar, Entries }
+export { Sidebar, Entries, MobileBar, NewEntry }
