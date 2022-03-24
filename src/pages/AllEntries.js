dec541237ad82433ed7cebe6bc41ca0ddb228409
Thomas Sankara <thomassankara74@gmail.com>
2022-03-24 18:04:14 +0300

    add text

--- src/pages/AllEntries.js
+++ src/pages/AllEntries.js
@@ -30,28 +30,34 @@ export default function AllEntries() {
               </div>
             ) : (
               entries.map(({ id, description, date }) => (
-                <article
-                  key={id}
-                  className="flex flex-col justify-center md:items-start md:justify-between md:flex-row"
-                >
-                  <div className="description rounded-md shadow md:mr-5 md:w-full">
-                    <small className="block text-xs px-5 pt-2">
-                      Date of entry: {date}
-                    </small>
-                    <p className="px-5 pb-2 text-white">{description}</p>
-                  </div>
+                <React.Fragment key={id}>
+                  <p>
+                    You have made {entries.length} diarry entries. You're
+                    amazing!
+                  </p>
+                  <article
+                    key={id}
+                    className="flex flex-col justify-center md:items-start md:justify-between md:flex-row"
+                  >
+                    <div className="description rounded-md shadow md:mr-5 md:w-full">
+                      <small className="block text-xs px-5 pt-2">
+                        Date of entry: {date}
+                      </small>
+                      <p className="px-5 pb-2 text-white">{description}</p>
+                    </div>
 
-                  <ul className="flex items-center justify-end md:justify-start mt-3 md:mt-0">
-                    <li>
-                      <button
-                        onClick={() => handleDelete(id)}
-                        className="py-1 px-4 rounded shadow bg-red-500 text-white border border-red-500 hover:bg-transparent transition-all duration-150 hover:text-red-500"
-                      >
-                        <AiOutlineDelete />
-                      </button>
-                    </li>
-                  </ul>
-                </article>
+                    <ul className="flex items-center justify-end md:justify-start mt-3 md:mt-0">
+                      <li>
+                        <button
+                          onClick={() => handleDelete(id)}
+                          className="py-1 px-4 rounded shadow bg-red-500 text-white border border-red-500 hover:bg-transparent transition-all duration-150 hover:text-red-500"
+                        >
+                          <AiOutlineDelete />
+                        </button>
+                      </li>
+                    </ul>
+                  </article>
+                </React.Fragment>
               ))
             )}
           </div>
