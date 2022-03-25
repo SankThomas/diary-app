bcfe9b351d6d5de7653b1114e6a00d0fbae6ecda
Thomas Sankara <thomassankara74@gmail.com>
2022-03-25 07:31:26 +0300

    scroll restoration component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Scroll } from "./components"
 import { Error, Home, Diarry, AllEntries } from "./pages"
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
+        <Scroll />
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/diarry" element={<Diarry />}></Route>
--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,7 @@
+import Scroll from "./Scroll"
 import Sidebar from "./Sidebar"
 import Entries from "./Entries"
 import MobileBar from "./MobileBar"
 import NewEntry from "./NewEntry"
 
-export { Sidebar, Entries, MobileBar, NewEntry }
+export { Scroll, Sidebar, Entries, MobileBar, NewEntry }
