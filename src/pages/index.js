f7ad084af3a77a391aaccb1afcfcad64fbf2e03b
Thomas Sankara <thomassankara74@gmail.com>
2022-09-03 23:15:50 +0300

    add markdown help page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,6 @@ import Home from "./Home/Home"
 import Diarry from "./Diarry"
 import AllEntries from "./AllEntries"
 import Error from "./Error"
+import Help from "./Help/Help"
 
-export { Home, Diarry, AllEntries, Error }
+export { Home, Diarry, AllEntries, Error, Help }
