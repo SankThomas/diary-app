9a63ef96d75eea072d133eb5ed931a1a077e44cb
sankthomas <thomassankara74@gmail.com>
2022-10-29 12:02:28 +0300

    add delete modal

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,8 @@
-import Scroll from "./Scroll"
-import Sidebar from "./Sidebar"
-import Entries from "./Entries"
-import MobileBar from "./MobileBar"
-import NewEntry from "./NewEntry"
+import Scroll from "./Scroll";
+import Sidebar from "./Sidebar";
+import Entries from "./Entries";
+import MobileBar from "./MobileBar";
+import NewEntry from "./NewEntry";
+import Modal from "./Modal";
 
-export { Scroll, Sidebar, Entries, MobileBar, NewEntry }
+export { Scroll, Sidebar, Entries, MobileBar, NewEntry, Modal };
