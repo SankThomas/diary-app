9fe65dad49b4e910b29dc7f4715c2711dadb0f13
sankthomas <thomassankara74@gmail.com>
2022-10-29 12:13:19 +0300

    upgrade to react v18

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import "./index.css"
-import App from "./App"
-import { EntriesContextProvider } from "./context/context"
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { EntriesContextProvider } from "./context/context";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+root.render(
   <React.StrictMode>
     <EntriesContextProvider>
       <App />
     </EntriesContextProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
-)
+  </React.StrictMode>
+);
