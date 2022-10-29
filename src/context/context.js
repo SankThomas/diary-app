050952538bc8db3a1a63260ccda8f55e363b7504
sankthomas <thomassankara74@gmail.com>
2022-10-29 12:03:33 +0300

    add modal states to context

--- src/context/context.js
+++ src/context/context.js
@@ -1,82 +1,90 @@
-import { createContext, useState, useEffect } from "react"
-import { v4 as uuidv4 } from "uuid"
-import { toast } from "react-toastify"
+import { createContext, useState, useEffect } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { toast } from "react-toastify";
 
 const EntriesContext = createContext({
   entries: [],
   text: "",
   isEditing: false,
+  openModal: false,
   getLocalStorage: () => {},
   handleSubmit: () => {},
   handleEdit: () => {},
   handleDelete: () => {},
-})
+});
 
 export const EntriesContextProvider = ({ children }) => {
   const getLocalStorage = () => {
-    let entries = localStorage.getItem("entries")
+    let entries = localStorage.getItem("entries");
 
     if (entries) {
-      return JSON.parse(localStorage.getItem("entries"))
+      return JSON.parse(localStorage.getItem("entries"));
     } else {
-      return []
+      return [];
     }
-  }
+  };
 
-  const [entries, setEntries] = useState(getLocalStorage())
-  const [text, setText] = useState("")
-  const [isEditing, setIsEditing] = useState(false)
+  const [entries, setEntries] = useState(getLocalStorage());
+  const [text, setText] = useState("");
+  const [isEditing, setIsEditing] = useState(false);
+  const [openModal, setOpenModal] = useState(false);
 
   // Submit function
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     if (!text) {
-      toast.info("Oops. Sorry, there's no entry in your diarry")
+      toast.info("Oops. Sorry, there's no entry in your diarry");
     } else {
       const newEntry = {
         id: uuidv4(),
         description: text,
         date: new Date().toLocaleString(),
-      }
-      setEntries([newEntry, ...entries])
-      setText("")
-      setIsEditing(false)
+      };
+      setEntries([newEntry, ...entries]);
+      setText("");
+      setIsEditing(false);
     }
-  }
+  };
 
   // Delete function
-  const handleDelete = (id) =>
-    setEntries(entries.filter((entry) => entry.id !== id))
+  const handleDelete = (id) => {
+    setEntries(entries.filter((entry) => entry.id !== id));
+    setOpenModal(false);
+  };
 
   // Edit function
   const handleEdit = (id) => {
-    const editingEntry = entries.find((entry) => entry.id === id)
-    setEntries(entries.filter((entry) => entry.id !== id))
-    setIsEditing(true)
-    setText(editingEntry.description)
-  }
+    const editingEntry = entries.find((entry) => entry.id === id);
+    setEntries(entries.filter((entry) => entry.id !== id));
+    setIsEditing(true);
+    setText(editingEntry.description);
+  };
+
+  // Open Modal function
 
   // Save to localStorage
   useEffect(() => {
-    localStorage.setItem("entries", JSON.stringify(entries))
-  }, [entries])
+    localStorage.setItem("entries", JSON.stringify(entries));
+  }, [entries]);
 
   const context = {
     entries,
     text,
     setText,
     isEditing,
+    openModal,
+    setOpenModal,
     handleSubmit,
     handleDelete,
     handleEdit,
-  }
+  };
 
   return (
     <EntriesContext.Provider value={context}>
       {children}
     </EntriesContext.Provider>
-  )
-}
+  );
+};
 
-export default EntriesContext
+export default EntriesContext;
